f57e06b2c409ac5cec23f97ced1b4d767550d33f
Nhat-Aegon <81905804+Nhat-Aegon@users.noreply.github.com>
2022-02-25 10:02:23 +0700

    Update snake_project.cpp

--- snake_project.cpp
+++ snake_project.cpp
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdlib.h>
+
+
 #include <iostream>
-#include<Windows.h> //Mot header cua Windows, chua cac ham trong windows api
+#include<windows.h> //Mot header cua Windows, chua cac ham trong windows api
 #include<thread>
 #include<conio.h>
-#include<stdlib.h>
 using namespace std;
 #define MAX_SIZE_SNAKE 20
 #define MAX_SIZE_FOOD 10
@@ -19,46 +21,47 @@ int SPEED;// Standing for level, the higher the level, the quicker the speed
 int HEIGH_CONSOLE, WIDTH_CONSOLE;// Width and height of console-screen
 int FOOD_INDEX; // current food-index
 int SIZE_SNAKE; // size of snake, initially maybe 6 units and maximum size maybe 32)
-int STATE; // State of snake: dead or alive 
+int STATE; // State of snake: dead or alive
 
 void FixConsoleWindow() { // ham vo hieu khoa viec user thay doi kich thuoc cua so console
-   HWND consoleWindow = GetConsoleWindow(); // HWND la 1 handle toi Window va la 1 kieu so dinh dang cua so Console, handle la 1 dinh dang chung ( thuong la con tro)
-                                            // duoc su dung de bieu dien 1 dieu gi do
-                                             // GetConsoleWindow la ham tra ve 1 handle toi window phu hop voi kieu du lieu duoc goi
-   long style = GetWindowLong(consoleWindow, GWL_STYLE); // lay thong tin 32-bit cua 1 cua so, GWL_STYLE lay thong tin kieu cua cua so console
-   style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME); // dieu chinh thong so cua style sao cho ngan nguoi dung phong to cua so hoac thay doi kich thuoc cua so
-   SetWindowLong(consoleWindow, GWL_STYLE, style); // thay doi thong tin cua 1 cua so, gia tri muon thay the
+	HWND consoleWindow = GetConsoleWindow(); // HWND la 1 handle toi Window va la 1 kieu so dinh dang cua so Console, handle la 1 dinh dang chung ( thuong la con tro)
+											// duoc su dung de bieu dien 1 dieu gi do
+												// GetConsoleWindow la ham tra ve 1 handle toi window phu hop voi kieu du lieu duoc goi
+	long style = GetWindowLong(consoleWindow, GWL_STYLE); // lay thong tin 32-bit cua 1 cua so, GWL_STYLE lay thong tin kieu cua cua so console
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME); // dieu chinh thong so cua style sao cho ngan nguoi dung phong to cua so hoac thay doi kich thuoc cua so
+	SetWindowLong(consoleWindow, GWL_STYLE, style); // thay doi thong tin cua 1 cua so, gia tri muon thay the
 }
 
 void GotoXY(int x, int y) { // ham chuyen con tro chuot toi toa do (x,y)
-        COORD coord; // bien luu toa do trong cua so console, toa do (0,0) nam tren cung ben trai
-        coord.X = x;
-        coord.Y = y;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); // GetStdHandle:tra ve 1 handle tuong ung voi thiet bi tieu chuan ( cu the la window console), ham con lai la di chuyen con tro toi toa do tren cua so console
+	COORD coord; // bien luu toa do trong cua so console, toa do (0,0) nam tren cung ben trai
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); // GetStdHandle:tra ve 1 handle tuong ung voi thiet bi tieu chuan ( cu the la window console), ham con lai la di chuyen con tro toi toa do tren cua so console
 }
 //Function to draw
 
 void DrawSnakeAndFoodBefore(char* str) {
-	GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);	//di chuyển tới vị trí x,y(food_index)
+	GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);
 	if (str == " ")
 		cout << str;
 
-	for (int i = 0; i < SIZE_SNAKE; i++) {	//chạy vòng lặp để vẽ rắn
-		GotoXY(snake[i].x, snake[i].y);	//di chuyển tới vị trí vòng lặp của rắn
+	for (int i = 0; i < SIZE_SNAKE; i++) {
+		GotoXY(snake[i].x, snake[i].y);
 		cout << ' ';
 	}
 }
 void DrawSnakeAndFoodAfter() {
-	GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);	//di chuyển tới vị trí x,y(food_index)
+	GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);
 	cout << ID[SIZE_SNAKE];
 
-	for (int i = 0; i < SIZE_SNAKE; i++) {	//chạy vòng lặp để vẽ rắn
-		GotoXY(snake[i].x, snake[i].y);	//di chuyển tới vị trí vòng lặp của rắn
-		cout << ID[i];
+	for (int i = SIZE_SNAKE - 1; i >= 0; i--) {
+		GotoXY(snake[i].x, snake[i].y);
+		cout << ID[SIZE_SNAKE - i - 1];
 	}
+	GotoXY(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y);
 }
 
-bool IsValid(int x, int y){
+bool IsValid(int x, int y) {
 
 	for (int i = 0; i < SIZE_SNAKE; i++) {
 
@@ -106,96 +109,95 @@ void Eat() {
 } //khi ran an moi thi do dai ran tang va vi tri moi duoc thay doi de tranh viec vi tri moi xuat hien tai vi tri con ran
 void ProcessDead()
 {
-	STATE = 0;												// Thay đổi trạng thái của rắn từ 1 sang 0 (sống sang chết), ngăn vòng lập vẽ rắn
-	GotoXY(0, HEIGH_CONSOLE + 2);							// Đi đến vị trí x = 0; y = (độ cao của console + 2) 
-	printf("Dead, type y to continue or anykey to exit");	/* In ra dong chữ cho người chơi chọn : y để chơi tiếp và phím khác để thoát, lấy dữ liệu
-															cho cấu trúc rẽ nhánh StartGame hoặc ExitGame*/
+	STATE = 0;
+	GotoXY(0, HEIGH_CONSOLE + 2);
+	printf("Dead, type y to continue or anykey to exit");
 }
 
-void MoveRight()											/* Hàm di chuyển sang phải */
+void MoveRight()
 {
-	if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE)		// Toạ độ x của đầu rắn bằng biên phải console => va chạm tường
+	if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE)
 	{
-		ProcessDead();										// => Rắn bị chết, gọi hàm ProcessDead
+		ProcessDead();
 	}
 	else
 	{
-		if (snake[SIZE_SNAKE - 1].x + 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) // Toạ độ đầu rắn trùng với toạ độ đồ ăn
+		if (snake[SIZE_SNAKE - 1].x + 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y)
 		{
-			Eat();											// => Ăn, gọi hàm Eat
+			Eat();
 		}
 		for (int i = 0; i < SIZE_SNAKE - 1; i++)
 		{
-			snake[i].x = snake[i + 1].x;					// Di chuyển các phần tử của rắn theo trục x 
-			snake[i].y = snake[i + 1].y;					// Di chuyển các phần tử của rắn theo trục y
-															// => Phần tử đứng sau lấy toạ độ của phần tử đứng trước => Xoá phần tử cuối)
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
 		}
-		snake[SIZE_SNAKE - 1].x++;							// Di chuyển đầu rắn sang phải 1 đơn vị
+		snake[SIZE_SNAKE - 1].x++;
 	}
 }
-void MoveLeft()												/* Hàm di chuyển sang trái */
+void MoveLeft()
 {
-	if (snake[SIZE_SNAKE - 1].x - 1 == 0)					// Toạ độ x của đầu rắn bằng biên trái của console => va chạm
+	if (snake[SIZE_SNAKE - 1].x - 1 == 0)
 	{
-		ProcessDead();										// => Rắn bị chết, gọi hàm ProcessDead
+		ProcessDead();
 	}
 	else {
-		if (snake[SIZE_SNAKE - 1].x - 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) // Toạ độ đầu rắn trùng với toạ độ đồ ăn
+		if (snake[SIZE_SNAKE - 1].x - 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y)
 		{
-			Eat();											// => Ăn, gọi hàm Eat
+			Eat();
 		}
 		for (int i = 0; i < SIZE_SNAKE - 1; i++)
 		{
-			snake[i].x = snake[i + 1].x;					// Di chuyển các phần tử của rắn theo trục x
-			snake[i].y = snake[i + 1].y;					// Di chuyển các phần tử của rắn theo trục y
-															// => Phần tử đứng sau lấy toạ độ của phần tử đứng trước => Xoá phần tử cuối)
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
 		}
-		snake[SIZE_SNAKE - 1].x--;							// Di chuyển đầu rắn sang trái 1 đơn vị
+		snake[SIZE_SNAKE - 1].x--;
 	}
 }
-void MoveDown()												/* Hàm di chuyển xuóng dưới */
+void MoveDown()
 {
-	if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE)		// Toạ độ y của đầu rắn bằng biên dưới console => va chạm
+	if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE)
 	{
-		ProcessDead();										// => Rắn bị chết, gọi hàm ProcessDead
+		ProcessDead();
 	}
 	else {
-		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y + 1 == food[FOOD_INDEX].y) // Toạ độ đầu rắn trùng với toạ độ đồ ăn
+		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y + 1 == food[FOOD_INDEX].y)
 		{
-			Eat();											// => Ăn, gọi hàm Eat
+			Eat();
 		}
 		for (int i = 0; i < SIZE_SNAKE - 1; i++)
 		{
-			snake[i].x = snake[i + 1].x;					// Di chuyển các phần tử của rắn theo trục x
-			snake[i].y = snake[i + 1].y;					// Di chuyển các phần tử của rắn theo trục y
-															// => Phần tử đứng sau lấy toạ độ của phần tử đứng trước => Xoá phần tử cuối)
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
 		}
-		snake[SIZE_SNAKE - 1].y++;							// Di chuyển đầu rắn xuống dưới 1 đơn vị
+		snake[SIZE_SNAKE - 1].y++;
 	}
 }
-void MoveUp()												/* Hàm di chuyển lên trên */
+void MoveUp()
 {
-	if (snake[SIZE_SNAKE - 1].y - 1 == 0)					// Toạ độ y của đầu rắn bằng biên trên console => va chạm
+	if (snake[SIZE_SNAKE - 1].y - 1 == 0)
 	{
-		ProcessDead();										// => Rắn bị chết, gọi hàm ProcessDead
+		ProcessDead();
 	}
 	else {
-		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y - 1 == food[FOOD_INDEX].y)	// Toạ độ đầu rắn trùng với toạ độ đồ ăn
+		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y - 1 == food[FOOD_INDEX].y)
 		{
-			Eat();											// => Ăn, gọi hàm Eat
+			Eat();
 		}
 		for (int i = 0; i < SIZE_SNAKE - 1; i++)
 		{
-			snake[i].x = snake[i + 1].x;					// Di chuyển các phần tử của rắn theo trục x 
-			snake[i].y = snake[i + 1].y;					// Di chuyển các phần tử của rắn theo trục y 
-															// => Phần tử đứng sau lấy toạ độ của phần tử đứng trước => Xoá phần tử cuối)
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
 		}
-		snake[SIZE_SNAKE - 1].y--;							// Di chuyển đầu rắn lên trên 1 đơn vị
+		snake[SIZE_SNAKE - 1].y--;
 	}
 }
 
 void ThreadFunc() {
-    while (true) {
+	while (true) {
 		if (STATE == 1) {//If my snake is alive
 			char* c = new char[2];
 			strcpy(c, " ");
@@ -218,49 +220,49 @@ void ThreadFunc() {
 			DrawSnakeAndFoodAfter();
 			Sleep(100 / SPEED);
 		}
-    }
+	}
 
 }
 void DrawBoard(int x, int y, int width, int height)
 {
-    // draw up and low walls
-    for (int i = 0; i <= width; i++)
-    {
-        GotoXY(x + i, y);
-        cout << 'X';
-        GotoXY(x + i, y + height);
-        cout << 'X';
-    }
-    // Draw 2 side walls
-    for (int i = 1; i <= height; i++)
-    {
-        GotoXY(x, y + i);
-        cout << 'X';
-        GotoXY(x + width, y + i);
-        cout << 'X';
-    }
+	// draw up and low walls
+	for (int i = 0; i <= width; i++)
+	{
+		GotoXY(x + i, y);
+		cout << 'X';
+		GotoXY(x + i, y + height);
+		cout << 'X';
+	}
+	// Draw 2 side walls
+	for (int i = 1; i <= height; i++)
+	{
+		GotoXY(x, y + i);
+		cout << 'X';
+		GotoXY(x + width, y + i);
+		cout << 'X';
+	}
 	GotoXY(width + 1, height + 1);
 }
 void ResetData() {
-   //Initialize the global values
+	//Initialize the global values
 	CHAR_LOCK = 'A', MOVING = 'D', SPEED = 1; FOOD_INDEX = 0, WIDTH_CONSOLE = 100,
-   HEIGH_CONSOLE = 20, SIZE_SNAKE = 6;
-   // Initialize default values for snake
-   snake[0] = { 10, 5 }; snake[1] = { 11, 5 }; // khoi tao giao tri cho ran
-  // dau ran nam o vi tri snake[5]
-   GenerateFood();
-   for (int i = 0; i < 6; i++)
-   {
-       snake[6-i-1].x = 15 - i;
-       snake[6-i-1].y = 5;
-   }
+		HEIGH_CONSOLE = 20, SIZE_SNAKE = 6;
+	// Initialize default values for snake
+	snake[0] = { 10, 5 }; snake[1] = { 11, 5 }; // khoi tao giao tri cho ran
+	// dau ran nam o vi tri snake[5]
+	GenerateFood();
+	for (int i = 0; i < 6; i++)
+	{
+		snake[6 - i - 1].x = 15 - i;
+		snake[6 - i - 1].y = 5;
+	}
 }
 void StartGame() {
-    system("cls"); // clear screen
-    ResetData(); // Intialize original data
-    DrawBoard(0, 0, WIDTH_CONSOLE, HEIGH_CONSOLE); // Draw game
-	
-    STATE = 1;//Start running Thread    
+	system("cls"); // clear screen
+	ResetData(); // Intialize original data
+	DrawBoard(0, 0, WIDTH_CONSOLE, HEIGH_CONSOLE); // Draw game
+
+	STATE = 1;//Start running Thread    
 }
 void ExitGame(HANDLE t) {
 	system("cls");
@@ -270,87 +272,8 @@ void ExitGame(HANDLE t) {
 void PauseGame(HANDLE t) {
 	SuspendThread(t);
 }
-void setColor(int color) // ham doi mau chu
+void newGame()
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
-
-void showCur(bool CursorVisibility) // ham hien/an con tro
-{
-	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO ConCurInf;
-
-	ConCurInf.dwSize = 10;
-	ConCurInf.bVisible = CursorVisibility;
-
-	SetConsoleCursorInfo(handle, &ConCurInf);
-}
-void main_menu() //xay dung menu
-{
-	int Set[] = { 7,7,7,7,7 }; // DEFAULT COLORS
-	int counter = 1;
-	char key;
-
-	while(true)
-	{
-		GotoXY(50, 5);
-		setColor(Set[0]);
-		cout << "NEW GAME";
-
-		GotoXY(50, 6);
-		setColor(Set[1]);
-		cout << "HIGHEST SCORE";
-
-		GotoXY(50, 7);
-		setColor(Set[2]);
-		cout << "CONTACT";
-
-		GotoXY(50, 8);
-		setColor(Set[3]);
-		cout << "LAST GAME";
-
-		key = _getch();
-
-		if ((key == 'W' || key == 'w') && (counter >= 2 && counter <= 4))
-		{
-			counter--;
-		}
-		if ((key == 'S' || key == 's') && (counter >= 1 && counter <= 3))
-		{
-			counter++;
-		}
-		if (key == '\r')
-		{
-			switch (counter)
-			{
-			case 1:
-			{
-				newGame();
-				return;
-				break;
-			}
-			break;
-			}
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			Set[i] = 7;
-		}
-
-		if (counter != 0)
-		{
-			Set[counter-1] = 12;
-		}
-
-
-	}
-	
-}
-void main()
-{
-	showCur(0);
-	FixConsoleWindow();
 	StartGame();
 	thread t1(ThreadFunc); //Create thread for snake
 	HANDLE handle_t1 = t1.native_handle(); //Take handle of thread
@@ -363,7 +286,7 @@ void main()
 				temp -= 32;
 		}
 		if (STATE == 1) {
-			if (temp == 'P') {
+			if (temp == ' ') {
 				PauseGame(handle_t1);
 			}
 			else if (temp == 27) {
@@ -391,5 +314,93 @@ void main()
 			}
 		}
 	}
-} 
+}
+
+void setColor(int color) // ham doi mau chu
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void showCur(bool CursorVisibility) // ham hien/an con tro
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO ConCurInf;
+
+	ConCurInf.dwSize = 10;
+	ConCurInf.bVisible = CursorVisibility;
+
+	SetConsoleCursorInfo(handle, &ConCurInf);
+}
+void main_menu() //xay dung menu
+{
+	int Set[] = { 7,7,7,7 }; // DEFAULT COLORS
+	int counter = 1;
+	char key;
+
+	while(true)
+	{
+		GotoXY(52, 5);
+		setColor(Set[0]);
+		cout << "NEW GAME";
+
+		GotoXY(50, 6);
+		setColor(Set[1]);
+		cout << "HIGHEST SCORE";
+
+		GotoXY(52, 7);
+		setColor(Set[2]);
+		cout << "CONTACT";
+
+		GotoXY(51, 8);
+		setColor(Set[3]);
+		cout << "LAST GAME";
+
+		key = _getch();
+
+		if ((key == 'W' || key == 'w'))
+		{
+			counter--;
+			if (counter == 0)
+				counter = 4;
+		}
+		if ((key == 'S' || key == 's'))
+		{
+			counter++;
+			if (counter > 4)
+				counter = 1;
+
+		}
+		if (key == '\r')
+		{
+			switch (counter)
+			{
+			case 1:
+			{
+				newGame();
+				return;
+				break;
+			}
+			break;
+			}
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			Set[i] = 7;
+		}
+
+		if (counter != 0)
+		{
+			Set[counter-1] = 12;
+		}
+	}
 	
+}
+
+void main()
+{
+	showCur(0);
+	FixConsoleWindow();
+	main_menu();
+}
+
