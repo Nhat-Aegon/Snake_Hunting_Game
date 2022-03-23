53e514bf585c3909de00478f0461895deb920323
minhkhoi204 <98089559+minhkhoi204@users.noreply.github.com>
2022-03-23 10:47:09 +0700

    Update MainMenu.cpp

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,1 +1,127 @@
+int LoadingAnimation() {
+	system("cls");
+	int i = 7;
+	int x = 30;
+	int y = 20;
+	int check = 0;
+	GotoXY(0, 0);
+	setColor(12);
+	cout << R"(
+			 _     _     _      _____  _  _      _____   ____  _      ____  _  __ _____
+			/ \ /|/ \ /\/ \  /|/__ __\/ \/ \  /|/  __/  / ___\/ \  /|/  _ \/ |/ //  __/
+			| |_||| | ||| |\ ||  / \  | || |\ ||| |  _  |    \| |\ ||| / \||   / |  \
+			| | ||| \_/|| | \||  | |  | || | \||| |_//  \___ || | \||| |-|||   \ |  /_
+			\_/ \|\____/\_/  \|  \_/  \_/\_/  \|\____\  \____/\_/  \|\_/ \|\_|\_\\____\
+		)";
+	GotoXY(30, 17);
+	setColor(14);
+	cout << "Please wait while the game is loading! ";
+	while (true)
+	{
+		GotoXY(x, y);
+		cout << static_cast<char>(219);
+		setColor(i);
+		if (check == 0) {
+			x++;
+		}
+		if (x == 90) {
+			break;
+		}
+		Sleep(100);
+	}
+	return 0;
+}
+
+void MainMenu() //xay dung menu // ten cu~: main_menu
+{
+	int i = 1;
+	int Set[] = { 12,7,7,7,7 }; // DEFAULT COLORS
+	int counter = 1;
+	char key;
+	//setColor(i);	
+
+	while (true)
+	{
+		i = !i;
+		GotoXY(0, 0);
+		if (i) {
+			SetColor(13);
+		}
+		else {
+			SetColor(10);
+		}
+		cout << R"(
+			 _     _     _      _____  _  _      _____   ____  _      ____  _  __ _____
+			/ \ /|/ \ /\/ \  /|/__ __\/ \/ \  /|/  __/  / ___\/ \  /|/  _ \/ |/ //  __/
+			| |_||| | ||| |\ ||  / \  | || |\ ||| |  _  |    \| |\ ||| / \||   / |  \  
+			| | ||| \_/|| | \||  | |  | || | \||| |_//  \___ || | \||| |-|||   \ |  /_ 
+			\_/ \|\____/\_/  \|  \_/  \_/\_/  \|\____\  \____/\_/  \|\_/ \|\_|\_\\____\
+                                                                           
+		)";
+		GotoXY(54, 9);
+		SetColor(Set[0]);
+		cout << "NEW GAME";
+
+		GotoXY(52, 10);
+		SetColor(Set[1]);
+		cout << "HIGHEST SCORE";
+
+		GotoXY(55, 11);
+		SetColor(Set[2]);
+		cout << "CONTACT";
+
+		GotoXY(54, 12);
+		SetColor(Set[3]);
+		cout << "LAST GAME";
+
+		GotoXY(56, 13);
+		SetColor(Set[4]);
+		cout << "QUIT";
+
+		key = _getch();
+
+		if ((key == 'W' || key == 'w'))
+		{
+			counter--;
+			if (counter == 0)
+				counter = 5;
+		}
+		if ((key == 'S' || key == 's'))
+		{
+			counter++;
+			if (counter > 5)
+				counter = 1;
+
+		}
+		if (key == '\r')
+		{
+			switch (counter)
+			{
+			case 1:
+			{
+				LoadingAnimation();
+				NewGame(1);
+				return;
+			}
+			case 4:
+			{
+				LoadingAnimation();
+				NewGame(2);
+				return;
+			}
+			break;
+			}
+		}
+
+		for (int i = 0; i < 5; i++)
+		{
+			Set[i] = 7;
+		}
+
+		if (counter != 0)
+		{
+			Set[counter - 1] = 12;
+		}
+	}
+}
 
