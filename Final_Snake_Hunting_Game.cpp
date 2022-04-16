0d77fae4a887503a12370daa13d33c4581d980a9
Nhat-Aegon <81905804+Nhat-Aegon@users.noreply.github.com>
2022-04-16 23:13:41 +0700

    Update Final_Snake_Hunting_Game.cpp

--- Final_Snake_Hunting_Game.cpp
+++ Final_Snake_Hunting_Game.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 int main()
 {
-	DATA *dataGame = new DATA();
+	DATA* dataGame = new DATA();
 	vector<POINT> obstacle;
 	obstacle.resize(0);
 	GATE* gate = new GATE();
 	ShowCur(0);
 	FixConsoleWindow();
-	MainMenu(dataGame,gate, obstacle);
+	MainMenu(dataGame, gate, obstacle);
 	delete gate;
 	return 0;
 }
-
 
